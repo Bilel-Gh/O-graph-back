a2f2fc359e6c50ba5097677ec54aeefdb035e358
Alexandre-prog <alexandreolivier@orange.fr>
2020-09-25 16:54:52 +0200

    login

--- index.js
+++ index.js
@@ -4,6 +4,9 @@ const bodyParser = require('body-parser');
 
 const commentRouter = require('./app/routers/commentRouter');
 const projectRouter = require('./app/routers/projectRouter');
+const userRouter = require('./app/routers/userRouter');
+
+const session = require('express-session');
 
 const express = require('express');
 
@@ -13,8 +16,26 @@ const app = express();
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.json());
 
+
+app.use(
+   session({
+     resave: false,
+     saveUninitialized: true,
+     secret: 'Iliade',
+     cookie: { secure: false },
+   })
+ );
+ 
+//  app.use((req, res, next) => {
+//    console.log('=== SESSION CHECKER ===');
+//    console.table(req.session);
+//    console.log('=== END OF SESSION CHECKER ===');
+//    next();
+//  });
+
 app.use(commentRouter);
 app.use(projectRouter);
+app.use(userRouter);
 
 const port = process.env.PORT || 3000;
 
